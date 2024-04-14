209646941bff58ee5cc218a03fd2a6ab7e505b09
Vasu Aggarwal <svasu.sv95@gmail.com>
2024-04-14 15:52:34 +0530

    Implemented fault tolerance in User-Service

--- UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.lcwd.user.service.controllers;
 
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @PostMapping("/")
     public ResponseEntity<User> createUser(@RequestBody User user){
         User newUser = this.userService.saveUser(user);
@@ -23,12 +28,25 @@ public class UserController {
     }
 
     @GetMapping("/")
+    @CircuitBreaker(name="allUserRating")
     public ResponseEntity<List<User>> getAllUsers(){
         return new ResponseEntity<>(this.userService.getAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/{uuid}")
+    @CircuitBreaker(name="singleUserRating", fallbackMethod = "singleUserRatingFallback")
     public ResponseEntity<User> getUserById(@PathVariable String uuid){
         return new ResponseEntity<>(this.userService.getUser(uuid), HttpStatus.OK);
+    }
+
+    //Creating fallback method for singleUserRating
+    public ResponseEntity<User> singleUserRatingFallback(String uuid, Exception exception){
+        logger.info("Fallback is executed because service is down : {}", exception.getMessage());
+        User user = User.builder()
+                .email("Dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created because service is down !!")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
